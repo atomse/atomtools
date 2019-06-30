a69c6afc9ee5ad2c5d17bf6b1723e3722320430d
Sky Zhang <sky.atomse@gmail.com>
2019-06-30 21:08:49 +0800

    add types

--- atomtools/__init__.py
+++ atomtools/__init__.py
@@ -3,12 +3,13 @@ independent chemical symbols
 """
 
 
-__version__ = '1.5.9'
+__version__ = '1.6.0'
 def version():
     return __version__
 
 from . import name, unit, geo
 from . import file, string, system
+from . import types
 
 from .status import Status
 
