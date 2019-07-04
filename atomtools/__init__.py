d15d07bd3227cc36258f1bd6582c9c7b1650133a
Sky Zhang <sky.atomse@gmail.com>
2019-07-04 10:33:27 +0800

    Add filetype analysis

--- atomtools/__init__.py
+++ atomtools/__init__.py
@@ -3,14 +3,14 @@ independent chemical symbols
 """
 
 
-__version__ = '1.6.1'
+__version__ = '1.7.0'
 def version():
     return __version__
 
 from . import name, unit, geo
 from . import file, string, system
 from . import types
-
+from .filetype import filetype
 from .status import Status
 
 
