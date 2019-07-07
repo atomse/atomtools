6f39801e812f97ca97871fa862df5bb5f303e2ea
Sky Zhang <sky.atomse@gmail.com>
2019-07-07 23:45:31 +0800

    New: cell_par

--- atomtools/geo.py
+++ atomtools/geo.py
@@ -349,3 +349,107 @@ def get_rotation_matrix(k, theta, radians = False, debug=False):
     R = np.identity(3)*math.cos(theta) + (1-math.cos(theta))*k_outer + \
         math.sin(theta)*np.matrix([[0, -kz, ky], [kz, 0, -kx], [-ky, kx, 0]])
     return np.array(R)
+
+
+def cellpar_to_cell(cellpar, ab_normal=(0, 0, 1), a_direction=None):
+    """Return a 3x3 cell matrix from cellpar=[a,b,c,alpha,beta,gamma].
+
+    Angles must be in degrees.
+
+    The returned cell is orientated such that a and b
+    are normal to `ab_normal` and a is parallel to the projection of
+    `a_direction` in the a-b plane.
+
+    Default `a_direction` is (1,0,0), unless this is parallel to
+    `ab_normal`, in which case default `a_direction` is (0,0,1).
+
+    The returned cell has the vectors va, vb and vc along the rows. The
+    cell will be oriented such that va and vb are normal to `ab_normal`
+    and va will be along the projection of `a_direction` onto the a-b
+    plane.
+
+    Example:
+
+    >>> cell = cellpar_to_cell([1, 2, 4, 10, 20, 30], (0, 1, 1), (1, 2, 3))
+    >>> np.round(cell, 3)
+    array([[ 0.816, -0.408,  0.408],
+           [ 1.992, -0.13 ,  0.13 ],
+           [ 3.859, -0.745,  0.745]])
+
+    """
+    if a_direction is None:
+        if np.linalg.norm(np.cross(ab_normal, (1, 0, 0))) < 1e-5:
+            a_direction = (0, 0, 1)
+        else:
+            a_direction = (1, 0, 0)
+
+    # Define rotated X,Y,Z-system, with Z along ab_normal and X along
+    # the projection of a_direction onto the normal plane of Z.
+    ad = np.array(a_direction)
+    Z = normed(ab_normal)
+    X = normed(ad - ad.dot(Z) * Z)
+    Y = np.cross(Z, X)
+
+    # Express va, vb and vc in the X,Y,Z-system
+    alpha, beta, gamma = 90., 90., 90.
+    if isinstance(cellpar, (int, float)):
+        a = b = c = cellpar
+    elif len(cellpar) == 1:
+        a = b = c = cellpar[0]
+    elif len(cellpar) == 3:
+        a, b, c = cellpar
+    else:
+        a, b, c, alpha, beta, gamma = cellpar
+
+    # Handle orthorhombic cells separately to avoid rounding errors
+    eps = 2 * np.spacing(90.0, dtype=np.float64)  # around 1.4e-14
+    # alpha
+    if abs(abs(alpha) - 90) < eps:
+        cos_alpha = 0.0
+    else:
+        cos_alpha = cos(alpha)
+    # beta
+    if abs(abs(beta) - 90) < eps:
+        cos_beta = 0.0
+    else:
+        cos_beta = cos(beta)
+    # gamma
+    if abs(gamma - 90) < eps:
+        cos_gamma = 0.0
+        sin_gamma = 1.0
+    elif abs(gamma + 90) < eps:
+        cos_gamma = 0.0
+        sin_gamma = -1.0
+    else:
+        cos_gamma = cos(gamma)
+        sin_gamma = sin(gamma)
+
+    # Build the cell vectors
+    va = a * np.array([1, 0, 0])
+    vb = b * np.array([cos_gamma, sin_gamma, 0])
+    cx = cos_beta
+    cy = (cos_alpha - cos_beta * cos_gamma) / sin_gamma
+    cz_sqr = 1. - cx * cx - cy * cy
+    assert cz_sqr >= 0
+    cz = math.sqrt(cz_sqr)
+    vc = c * np.array([cx, cy, cz])
+
+    # Convert to the Cartesian x,y,z-system
+    abc = np.vstack((va, vb, vc))
+    T = np.vstack((X, Y, Z))
+    cell = abc.dot(T)
+
+    return cell
+
+
+
+def cell_abc_alpha_beta_gamma_to_cartesion(params):
+    if len(params) == 3:
+        return np.diag(params)
+    elif len(params) == 6:
+        return cellpar_to_cell(params)
+    elif np.array(params).shape == (3, 3):
+        a, b, c, alpha, beta, gamma = params
+        return a, b, c, alpha, beta. gamma
+    else:
+        raise ValueError("params type error")
