0bb09d49904c5447679310d068827209c434b8be
Sky Zhang <sky.atomse@gmail.com>
2019-10-19 22:07:58 +0800

    update

--- atomtools/__init__.py
+++ atomtools/__init__.py
@@ -3,12 +3,8 @@ independent chemical symbols
 """
 
 
-__version__ = '1.8.1'
+__version__ = '1.8.5'
+
+
 def version():
     return __version__
-
-# from . import name, unit, geo
-# from . import file, string, system
-# from . import types
-# from .filetype import filetype, list_supported_formats, support_multiframe
-# from .status import Status
